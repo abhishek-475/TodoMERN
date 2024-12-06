1f5bb0874561a0413c589a658217401965755979
abhishek-475 <61498092+abhishek-475@users.noreply.github.com>
2024-12-06 20:51:43 +0530

    second

--- src/Services/AllApi.js
+++ src/Services/AllApi.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const base_url = "https://todoserver-izor.onrender.com/";
+const base_url = "https://todoserver-izor.onrender.com";
 
 export const addTask = async (data) => {
-  return await axios.post(`${base_url}add`, data);
+  return await axios.post(`${base_url}/tasks`, data);
 };
 
 export const getTasks = async () => {
-  return await axios.get(`${base_url}all`);
+  return await axios.get(`${base_url}/tasks`);
 };
 
 export const updateTask = async (id, status) => {
-  return await axios.put(`${base_url}update/${id}`, { status });
+  return await axios.put(`${base_url}/tasks/${id}`, { status });
 };
 
 export const deleteTask = async (id) => {
-  return await axios.delete(`${base_url}delete/${id}`);
+  return await axios.delete(`${base_url}/tasks/${id}`);
 };
 
